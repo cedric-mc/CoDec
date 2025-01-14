59bd0c011f8860e02d296c43e8c17b68d2dbe036
Cédric Mariya Constantine <cedric.mc11@gmail.com>
2025-01-14 19:07:17 +0100

    Ajout d'un Makefile et des fichiers source pour le traitement d'images avec libg2x

---
+++ Makefile
@@ -0,0 +1,61 @@
+# Variables générales
+CC  = gcc
+STD = -std=c17
+
+PFLAGS = $(incG2X) -I./include
+CFLAGS = -Wpointer-arith -Wall
+
+# Optimisation ou débogage
+ifeq ($(GDB),1)
+  CFLAGS += -g
+  EXT    = .gdb
+else
+  CFLAGS += -O2
+  EXT    =
+endif
+
+# Sélection de la bonne version de libg2x*.so
+LFLAGS = $(libG2X)$(EXT)
+
+# Dossier source
+SRC = src/
+
+# Liste des exécutables à générer
+ALL = main
+
+# Règle spécifique pour l'exécutable 'main'
+main: difimg.o main.o
+	$(CC) $^ $(LFLAGS) -o $@
+
+# Règle générique pour créer un fichier .o à partir d'un fichier .c
+%.o : $(SRC)%.c
+	@echo "Compilation de $@"
+	@$(CC) $(STD) $(CFLAGS) $(PFLAGS) -c $< -o $@
+	@echo "------------------------"
+
+# Règle générique pour assembler un exécutable à partir d'un fichier .o
+% : %.o
+	@echo "Assemblage [$^] -> $@"
+	@$(CC) $^ $(LFLAGS) -o $@
+	@echo "------------------------"
+
+# Génération de tous les exécutables listés dans ALL
+all : $(ALL)
+
+# Règles de nettoyage
+.PHONY : clean cleanall ?
+
+clean :
+	@rm -f *.o
+
+cleanall :
+	@rm -f *.o $(ALL)
+
+# Informations sur les paramètres de compilation
+? :
+	@echo "--------- Compilation Informations ---------"
+	@echo "  Compilateur : $(CC)"
+	@echo "  Standard    : $(STD)"
+	@echo "  PFLAGS      : $(PFLAGS)"
+	@echo "  CFLAGS      : $(CFLAGS)"
+	@echo "  LFLAGS      : $(LFLAGS)"
---
+++ src/main.c
@@ -0,0 +1,96 @@
+#include <g2x.h>
+#include <difimg.h>
+
+/* variables globales */
+/* récupère et segmente le chemin vers l'image : pathname/rootname.extname */
+static char *rootname = NULL;
+static char *pathname = NULL;
+static char *extname = NULL;
+G2Xpixmap *img = NULL;
+static G2Xpixmap *visu = NULL, *orig = NULL;
+DiffImg dif = {0};
+
+/* paramètres d'interaction */
+static bool SWAP = true; /* affichage : false->original  true->copie */
+
+/*! fonction d'initialisation !*/
+void init(void)
+{
+    g2x_PixmapPreload(img);
+    int w = img->width, h = img->height;
+    difalloc(&dif, w, h); /* Allocation */
+    pixtodif(img, &dif); /* Création */
+    g2x_PixmapAlloc(&visu, w, h, 1, 255); /* Allocation */
+    diftovisu(&dif, visu); /* Création */
+    g2x_PixmapAlloc(&orig, w, h, 1, 255); /* Allocation */
+    diftopix(&dif, orig); /* Création */
+}
+
+/* passe la copie en négatif */
+static void self_negate(void)
+{
+    for (uchar *p = visu->map; p < visu->end; p++) *p = ~*p;
+}
+
+/*! fonction de contrôle      !*/
+void ctrl(void)
+{
+    // selection de la fonte : ('n':normal,'l':large,'L':LARGE),('n':normal,'b':bold),('l':left, 'c':center, 'r':right)
+    g2x_SetFontAttributes('l', 'b', 'c');
+    g2x_CreatePopUp("NEG", self_negate, "négatif sur la copie");
+    g2x_CreateSwitch("O/DIF", &SWAP, "affiche l'original ou la visuelle");
+}
+
+void evts(void)
+{
+
+}
+
+/*! fonction de dessin        !*/
+void draw(void)
+{
+    switch (SWAP)
+    {
+    case false:
+        g2x_PixmapRecall(img, true); /* rappel de l'image originale */
+        break;
+    case true:
+        g2x_PixmapShow(visu, true); /* affiche la copie de travail */
+        break;
+    }
+}
+
+/*! fonction de sortie        !*/
+static void quit(void)
+{
+    /* rien à faire ici, l'image <img> est libérée automatiquement */
+}
+
+/*!***************************!*/
+/*! fonction principale       !*/
+/*!***************************!*/
+int main(int argc, char *argv[])
+{
+    if (argc < 2)
+    {
+        fprintf(stderr, "\e[1musage\e[0m : %s <path_to_image>\n", argv[0]);
+        return 1;
+    }
+
+    /* cas particulier :
+     * ouvre directement sur une image
+     * -> le pixmap est allouée par la lib. (et libéré également).
+     */
+    if (!(img = g2x_InitImage(argv[1], &pathname, &rootname, &extname)))
+    {
+        fprintf(stderr, "\e[1m%s\e[0m : cannot read %s \n", argv[0], argv[1]);
+        return 1;
+    }
+    g2x_SetInitFunction(init); /*! fonction d'initialisation !*/
+    g2x_SetCtrlFunction(ctrl); /*! fonction de controle      !*/
+    g2x_SetEvtsFunction(evts); /*! fonction d'événements     !*/
+    g2x_SetDrawFunction(draw); /*! fonction de dessin        !*/
+    g2x_SetExitFunction(quit); /*! fonction de sortie        !*/
+
+    return g2x_MainStart();
+}
